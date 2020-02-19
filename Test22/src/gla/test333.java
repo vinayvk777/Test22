ceff2b62a7be10c5ec0cc73e86b8efd4ce9238ca
vinay.singh <vinay.singh_cs16@gla.ac.in>
2020-02-19 16:48:59 +0530

    hi

--- Test22/src/gla/test333.java
+++ Test22/src/gla/test333.java
@@ -5,6 +5,9 @@ public class test333 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hiii");
+		
+		System.out.println("hiii");
+		System.out.println("hiii");
 	}
 
 }
